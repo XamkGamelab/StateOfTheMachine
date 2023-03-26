37864183276f0b9106c95b04d5b3ddced8f1122c
p3v1Xamk <pekka.vilpponen@xamk.fi>
2023-03-26 11:00:42 +0300

    Added InputController to test instantiating fear spreading lightning bolt.

--- Assets/Scripts/Core/ApplicationController.cs
+++ Assets/Scripts/Core/ApplicationController.cs
@@ -2,6 +2,7 @@ using System.Linq;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UniRx;
 
 /// <summary>
 /// Instantiate and initialize necessary objects.
@@ -18,7 +19,38 @@ public class ApplicationController : SingletonMono<ApplicationController>
     #region public
     public void Init()
     {
+        //Subscribe to input (not need to dispose, this is singleton)
+        InputController.Instance.MouseLeftDown.Subscribe(b => HandleMouseDown(b));
+    }
+    #endregion
 
+    #region Private
+    
+    private void HandleMouseDown(bool down)
+    {
+        if (down)
+        {
+            
+            Vector3? worldHitPoint = ScreenPointRaycast(Input.mousePosition)?.point ?? null;
+            
+            if (worldHitPoint.HasValue)
+                InstantiateLightning(worldHitPoint.Value);
+        }
+    }
+
+    private void InstantiateLightning(Vector3 worldPoint)
+    {
+        Debug.DrawRay(worldPoint, Vector3.up * 10f, Color.blue, 1f);
+    }
+
+    private RaycastHit? ScreenPointRaycast(Vector3 screenPoint)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(screenPoint);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit))
+            return hit;
+        else
+            return null;
     }
     #endregion
 }
---
+++ Assets/Scripts/Core/InputController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using UniRx;
+
+/// <summary>
+/// Singleton class that filter Unity input to reactive properties.
+/// </summary>
+public class InputController : SingletonMono<InputController>
+{
+    //Terrain drawing
+    public ReactiveProperty<bool> MouseLeftDown = new ReactiveProperty<bool>();
+    public ReactiveProperty<bool> MouseRightDown = new ReactiveProperty<bool>();
+    public ReactiveProperty<bool> MouseMiddleDown = new ReactiveProperty<bool>();
+    public ReactiveProperty<float> MouseScroll = new ReactiveProperty<float>();
+    public ReactiveProperty<bool> ShiftDown = new ReactiveProperty<bool>();
+    public ReactiveProperty<bool> CtrlDown = new ReactiveProperty<bool>();
+    public ReactiveProperty<bool> AltDown = new ReactiveProperty<bool>();
+    public ReactiveProperty<bool> EscDown = new ReactiveProperty<bool>();
+
+    //FPS controls
+    public ReactiveProperty<float> Horizontal = new ReactiveProperty<float>();
+    public ReactiveProperty<float> Vertical = new ReactiveProperty<float>();
+    public ReactiveProperty<MouseMovement> MouseMove = new ReactiveProperty<MouseMovement>();
+    public ReactiveProperty<float> MouseHorizontal = new ReactiveProperty<float>();
+    public ReactiveProperty<float> MouseVertical = new ReactiveProperty<float>();
+    public ReactiveProperty<bool> Fire1 = new ReactiveProperty<bool>();
+    public ReactiveProperty<bool> Jump = new ReactiveProperty<bool>();
+    public ReactiveProperty<bool> Run = new ReactiveProperty<bool>();
+    public ReactiveProperty<bool> Anykey = new ReactiveProperty<bool>();
+
+    #region Unity
+    private void Update()
+    {
+        //Terrain drawing input
+        MouseLeftDown.Value = Input.GetMouseButton(0);
+        MouseRightDown.Value = Input.GetMouseButton(1);
+        MouseMiddleDown.Value = Input.GetMouseButton(2);
+        MouseScroll.Value = Input.GetAxis("Mouse ScrollWheel");
+        ShiftDown.Value = Input.GetKey(KeyCode.LeftShift);
+        CtrlDown.Value = Input.GetKey(KeyCode.LeftControl);
+        AltDown.Value = Input.GetKey(KeyCode.LeftAlt);
+        EscDown.Value = Input.GetKey(KeyCode.Escape);
+
+        //Fps input
+        Horizontal.Value = Input.GetAxis("Horizontal");
+        Vertical.Value = Input.GetAxis("Vertical");
+
+        MouseHorizontal.Value = Input.GetAxis("Mouse X");
+        MouseVertical.Value = Input.GetAxis("Mouse Y");
+
+        MouseMove.Value = new MouseMovement(Input.mousePosition, new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")));
+
+        //Buttons
+
+        if (Input.GetButtonDown("Fire1"))
+            Fire1.Value = true;
+
+        if (Input.GetButtonUp("Fire1"))
+            Fire1.Value = false;
+
+        if (Input.GetButtonDown("Jump"))
+            Jump.Value = true;
+
+        if (Input.GetButtonUp("Jump"))
+            Jump.Value = false;
+    }
+    #endregion
+}
+
+public class MouseMovement
+{
+    public Vector2 Position;
+    public Vector2 Delta;
+
+    public MouseMovement() { }
+
+    public MouseMovement(Vector2 _pos, Vector2 _delta)
+    {
+        Position = _pos;
+        Delta = _delta;
+    }
+}
